ea07112fe22d748c13df4e9b22b47a96732245f9
Jan Pánek <u19.418@pslib.cloud>
2021-10-20 11:39:03 +0200

    commit

--- XDDDD/Pages/Index.cshtml.cs
+++ XDDDD/Pages/Index.cshtml.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using XDDDD.Model;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace XDDDD.Pages
 {
@@ -15,6 +17,9 @@ namespace XDDDD.Pages
         private readonly GalleryDbContext dbContext;
 
         public IEnumerable<Album> Albums { get; set; }
+        [BindProperty]
+        [Required]
+        public string NewAlbumName { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger, GalleryDbContext dbContext)
         {
@@ -24,7 +29,20 @@ namespace XDDDD.Pages
 
         public void OnGet()
         {
-            Albums = dbContext.Albums.Take(10).AsEnumerable();
+            Albums = dbContext.Albums.Take(10)
+                .Include(a => a.Pictures).AsEnumerable();
+        }
+
+        public ActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var data = NewAlbumName;
+            dbContext.Albums.Add(new Album { Name = NewAlbumName, CreatedAt = DateTime.Now });
+            dbContext.SaveChanges();
+            return RedirectToPage("Index");
         }
     }
 }
